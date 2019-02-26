bbbb337e0948f3e782de45e4f213868c5a03c79e
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 13:18:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 // Instantiate the client.
-$s3 = new Aws\S3\S3Client([
-	'region'  => 'us-east-1',
-	'version' => 'latest',
-	'credentials' => [
-	    'key'    => "ASIAYZ4HZNCRTCELYS6O",
-	    'secret' => "qsOVHY6zraQhuPdcKXKb5pvN8BHiCogTBdSQYaU1",
-	]
-]);
+$s3 = Aws\S3\S3Client::factory();
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
