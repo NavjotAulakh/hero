a32bfc09b1e82b2a275133ec3b3bcd86d49a541f
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 16:47:04 -0500

    Update location.php

--- location.php
+++ location.php
@@ -1,47 +1,42 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-  <head>
-    <title>Event Locater</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      html, body, #map_canvas {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-      }
-    </style>
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script src="../geolocation-marker.js"></script>
-    <script>
-      var map, GeoMarker;
-      function initialize() {
-        var mapOptions = {
-          zoom: 17,
-          center: new google.maps.LatLng(-34.397, 150.644),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById('map_canvas'),
-            mapOptions);
-        GeoMarker = new GeolocationMarker();
-        GeoMarker.setCircleOptions({fillColor: '#808080'});
-        google.maps.event.addListenerOnce(GeoMarker, 'position_changed', function() {
-          map.setCenter(this.getPosition());
-          map.fitBounds(this.getBounds());
-        });
-        google.maps.event.addListener(GeoMarker, 'geolocation_error', function(e) {
-          alert('There was an error obtaining your position. Message: ' + e.message);
-        });
-        GeoMarker.setMap(map);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-      if(!navigator.geolocation) {
-        alert('Your browser does not support geolocation');
-      }
-    </script>
-  </head>
-  <body>
-    <a href="https://eventprompter.herokuapp.com/index.php">Event SMS</a>
-    <div id="map_canvas"></div>
-  </body>
+   <head>
+      <script type = "text/javascript">
+         function showLocation(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var latlongvalue = position.coords.latitude + ","
+                              + position.coords.longitude;
+            var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+                          +latlongvalue+"&zoom=14&size = 400x300&key =
+                          AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+            document.getElementById("mapholder").innerHTML =
+            "<img src ='"+img_url+"'>";
+         }
+         function errorHandler(err) {
+            if(err.code == 1) {
+               alert("Error: Access is denied!");
+            }else if( err.code == 2) {
+               alert("Error: Position is unavailable!");
+            }
+         }
+         function getLocation(){
+            if(navigator.geolocation){
+               // timeout at 60000 milliseconds (60 seconds)
+               var options = {timeout:60000};
+               navigator.geolocation.getCurrentPosition
+               (showLocation, errorHandler, options);
+            }else{
+               alert("Sorry, browser does not support geolocation!");
+            }
+         }
+      </script>
+   </head>
+   <body>
+     <a href="https://eventprompter.herokuapp.com/index.php">Event SMS</a>
+      <div id="mapholder"></div>
+      <form>
+         <input type="button" onclick="getLocation();" value="Your Location"/>
+      </form>
+   </body>
 </html>
