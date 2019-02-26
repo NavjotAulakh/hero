f0198e3f258b7d19552397c97fc06fe06a5682a7
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 12:23:51 -0500

    Update sendEventSMS.php

--- sendEventSMS.php
+++ sendEventSMS.php
@@ -1,17 +1,36 @@
 <?php
-require 'vendor/autoload.php';
-$sdk = new Aws\Sns\SnsClient([
-    'region'  => 'us-east-1',
-    'version' => 'latest',
-    'credentials' => ['key' => 'ASIAYZ4HZNCR235B5CXF', 'secret' => '1tX2UHCCV5ifT1wh3WUkrfoKiCPzhxmdd7O1FQ5x']
-  ]);
-$result = $sdk->publish([
-    'Message' => 'Event SMS Test Message',
-    'PhoneNumber' => '+16475334532',
-    'MessageAttributes' => ['AWS.SNS.SMS.SenderID' => [
-         'DataType' => 'String',
-         'StringValue' => 'WebNiraj'
-      ]
-  ]]);
-print_r( $result );
+require './vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$params = array(
+    'credentials' => array(
+        'key' => 'ASIAYZ4HZNCR235B5CXF',
+        'secret' => '1tX2UHCCV5ifT1wh3WUkrfoKiCPzhxmdd7O1FQ5x',
+    ),
+    'region' => 'us-east-1', // < your aws from SNS Topic region
+    'version' => 'latest'
+);
+$sns = new \Aws\Sns\SnsClient($params);
+
+$args = array(
+    "MessageAttributes" => [
+                'AWS.SNS.SMS.SenderID' => [
+                    'DataType' => 'String',
+                    'StringValue' => 'EventPrompter'
+                ],
+                'AWS.SNS.SMS.SMSType' => [
+                    'DataType' => 'String',
+                    'StringValue' => 'Transactional'
+                ]
+            ],
+    "Message" => "Event Message",
+    "PhoneNumber" => "6475334532"
+);
+
+
+$result = $sns->publish($args);
+echo "<pre>";
+var_dump($result);
+echo "</pre>";
 ?>
