8a11f8e3c699c31a5182b6606654ac712392db43
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 13:47:42 -0500

    Update sendEventSMS.php

--- sendEventSMS.php
+++ sendEventSMS.php
@@ -2,42 +2,30 @@
 require './vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-use Aws\Credentials\CredentialProvider;
+use Aws\Sns\SnsClient;
 
-$profile = 'sns-reminders';
-$path = 'credentials';
+$sns = \Aws\Sns\SnsClient::factory(array(
+    'region' => 'us-east-1',
+    'version'  => 'latest',
+));
 
-$provider = CredentialProvider::ini($profile, $path);
-$provider = CredentialProvider::memoize($provider);
-
-$sdk = new Aws\Sdk(['credentials' => $provider]);
-$sns = $sdk->createSns([
-//        'profile' => $profile,
-        'region'  => 'us-east-1',
-        'version' => 'latest',
-]);
-
-
-// You just need to publish it and include the `PhoneNumber` parameter
-$snsClientResult = $sns->publish([
-    'Message' => 'Event Message',
-    'PhoneNumber' => '+16475334532',
-    'MessageStructure' => 'SMS',
+$result = $sns->publish([
+    'Message' => 'Testing event message', // REQUIRED
     'MessageAttributes' => [
         'AWS.SNS.SMS.SenderID' => [
-            'DataType' => 'String',
-            'StringValue' => 'Nav',
+            'DataType' => 'String', // REQUIRED
+            'StringValue' => 'Nav'
         ],
         'AWS.SNS.SMS.SMSType' => [
-            'DataType' => 'String',
-            'StringValue' => 'Promotional', // Transactional
+            'DataType' => 'String', // REQUIRED
+            'StringValue' => 'Transactional' // or 'Promotional'
         ]
-    ]
+    ],
+    'PhoneNumber' => '+16475334532',
 ]);
-
-// Get the response
+print_r($result);
 
 echo "<pre>";
-var_dump($snsClientResult['MessageId']);
+var_dump($result);
 echo "</pre>";
 ?>
