acceade9b4d4f9ba617c2dd33feb98fb9eaf6478
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 16:41:22 -0500

    Update location.php

--- location.php
+++ location.php
@@ -1,156 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
- 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>event Location</title>
-	
-	<style>
-	body{
-		font-family:arial;
-		font-size:.8em;
-	}
-	
-	input[type=text]{
-		padding:0.5em;
-		width:20em;
-	}
-	
-	input[type=submit]{
-		padding:0.4em;
-	}
-	
-	#gmap_canvas{
-		width:100%;
-		height:30em;
-	}
-	
-	#map-label,
-	#address-examples{
-		margin:1em 0;
-	}
-	</style>
-
-</head>
-<body>
-
-<?php
-if($_POST){
-
-	// get latitude, longitude and formatted address
-	$data_arr = geocode($_POST['address']);
-
-	// if able to geocode the address
-	if($data_arr){
-		
-		$latitude = $data_arr[0];
-		$longitude = $data_arr[1];
-		$formatted_address = $data_arr[2];
-					
-	?>
-
-	<!-- google map will be shown here -->
-	<div id="gmap_canvas">Loading map...</div>
-	<div id='map-label'>Map shows approximate location.</div>
-
-	<!-- JavaScript to show google map -->
-	<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyDC6bjwHN4cqoHKHc53z5osHbIDabdTEZs"></script>    
-	<script type="text/javascript">
-		function init_map() {
-			var myOptions = {
-				zoom: 14,
-				center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>),
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
-			marker = new google.maps.Marker({
-				map: map,
-				position: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>)
-			});
-			infowindow = new google.maps.InfoWindow({
-				content: "<?php echo $formatted_address; ?>"
-			});
-			google.maps.event.addListener(marker, "click", function () {
-				infowindow.open(map, marker);
-			});
-			infowindow.open(map, marker);
-		}
-		google.maps.event.addDomListener(window, 'load', init_map);
-	</script>
-
-	<?php
-
-	// if unable to geocode the address
-	}else{
-		echo "No map found.";
-	}
-}
-?>
-
-<div id='address-examples'>
-	<div>Address examples:</div>
-	<div>1. G/F Makati Cinema Square, Pasong Tamo, Makati City</div>
-	<div>2. 80 E.Rodriguez Jr. Ave. Libis Quezon City</div>
-</div>
-
-<!-- enter any address -->
-<form action="" method="post">
-	<input type='text' name='address' placeholder='Enter event address Here' />
-	<input type='submit' value='Locate' />
-</form>
-
-<?php
-
-// function to geocode address, it will return false if unable to geocode address
-function geocode($address){
-
-	// url encode the address
-	$address = urlencode($address);
-	
-	// google map geocode api url
-	$url = "https://maps.google.com/maps/api/geocode/json?address={$address}&key=AIzaSyDC6bjwHN4cqoHKHc53z5osHbIDabdTEZs";
-
-	// get the json response
-	$resp_json = file_get_contents($url);
-	
-	// decode the json
-	$resp = json_decode($resp_json, true);
-
-	// response status will be 'OK', if able to geocode given address 
-	if($resp['status']='OK'){
-
-		// get the important data
-		$lati = $resp['results'][0]['geometry']['location']['lat'];
-		$longi = $resp['results'][0]['geometry']['location']['lng'];
-		$formatted_address = $resp['results'][0]['formatted_address'];
-		
-		// verify if data is complete
-		if($lati && $longi && $formatted_address){
-		
-			// put the data in the array
-			$data_arr = array();			
-			
-			array_push(
-				$data_arr, 
-					$lati, 
-					$longi, 
-					$formatted_address
-				);
-			
-			return $data_arr;
-			
-		}else{
-			return false;
-		}
-		
-	}else{
-		return false;
-	}
-}
-?>
-
-</body>
+<html>
+  <head>
+    <title>Geolocation Marker Example Usage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      html, body, #map_canvas {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../geolocation-marker.js"></script>
+    <script>
+      var map, GeoMarker;
+      function initialize() {
+        var mapOptions = {
+          zoom: 17,
+          center: new google.maps.LatLng(-34.397, 150.644),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map_canvas'),
+            mapOptions);
+        GeoMarker = new GeolocationMarker();
+        GeoMarker.setCircleOptions({fillColor: '#808080'});
+        google.maps.event.addListenerOnce(GeoMarker, 'position_changed', function() {
+          map.setCenter(this.getPosition());
+          map.fitBounds(this.getBounds());
+        });
+        google.maps.event.addListener(GeoMarker, 'geolocation_error', function(e) {
+          alert('There was an error obtaining your position. Message: ' + e.message);
+        });
+        GeoMarker.setMap(map);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      if(!navigator.geolocation) {
+        alert('Your browser does not support geolocation');
+      }
+    </script>
+  </head>
+  <body>
+    <div id="map_canvas"></div>
+  </body>
 </html>
