07ad6d265617a4f0c0047d914ab2989188ee60a3
Navjot Aulakh <navjot.aulakh@uoit.net>
2019-02-26 23:35:16 -0500

    Update vision.php

--- vision.php
+++ vision.php
@@ -5,6 +5,7 @@
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 </head>
 <body ng-app="ocrApp" ng-controller="ocrController">
+	<a href="https://eventprompter.herokuapp.com/index.php">Event - Main Page</a>
 	<canvas id="uploadedPic">  Image Results	</canvas>
 	<p>
 		<input type="file" onchange="angular.element(this).scope().uploadPicture( this)">
